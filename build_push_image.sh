958a11522a61129315d7ce2d782cc7ff734b5cfe
Andang Vijayanto Utomo <andangvijayantoutomo@gmail.com>
2023-12-06 22:47:36 +0700

    add docker publish file

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-# Perintah untuk membuat Docker image dari Dockerfile
-docker build -t item-app:v1 .
-
-# Melihat daftar image di lokal
-docker images
-
 # Login info
-echo "..Login to Docker Hub.."
+echo "=== Login ke ghcr.io ==="
 echo "Username: "
 read username
 echo "Password: "
 read -s password
 
-# Mengubah nama image agar sesuai dengan format Docker Hub
+# Perintah untuk membuat Docker image dari Dockerfile
+docker build -t ghcr.io/$username/item-app:v1 .
+
+# Melihat daftar image di lokal
+docker images | grep item-app
+
+# Tag image
 docker tag item-app:v1 $username/item-app:v1
 
-# Login ke Docker Hub
+# Login ke ghcr.io
 docker login -u $username -p $password
 
-# Mengunggah image ke Docker Hub
-docker push $username/item-app:v1
+# Mengunggah image ke ghcr.io
+docker push ghcr.io/$username/item-app:v1
 
+# Logout dari ghcr.io
+docker logout ghcr.io
+
+docker-compose up -d > log.txt 2>&1
